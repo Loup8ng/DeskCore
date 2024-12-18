3154444ac2a01e117e9fd067bdcfe074ed7c0644
ryno57 <tristanou.girardin@gmail.com>
2024-12-18 11:23:27 +0100

    fenetre de base

--- Script/DeskCore.py
+++ Script/DeskCore.py
@@ -1,3 +1,15 @@
-import customtkinter
-ligne de code
-e = 2
+import customtkinter as ctk
+
+ctk.set_appearance_mode("Dark")
+
+app = ctk.CTk()
+app.title("DeskCore")
+app.geometry("800x600")
+
+frame_1 = ctk.CTkFrame(app, width=200)
+frame_1.pack(side="left", fill="y", padx=10, pady=10)
+
+frame_2 = ctk.CTkFrame(app)
+frame_2.pack(side="right", fill="both", expand=True, padx=10, pady=10)
+
+app.mainloop()
