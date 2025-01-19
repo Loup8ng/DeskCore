10f4eb255ac215eb5db41991e4f99b269c01e4ce
ryno57 <tristanou.girardin@gmail.com>
2025-01-19 19:39:52 +0100

    Documentation

--- Scripts/Scripts_windows/Notes_app.py
+++
@@ -1,6 +0,0 @@
-import customtkinter as ctk
-
-class NotesApp:
-    def __init__(self, parent):
-        self.text_area = ctk.CTkTextbox(parent, width=100, height=800)
-        self.text_area.pack(fill="both", expand=True, padx=5, pady=5)
