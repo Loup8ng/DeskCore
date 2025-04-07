4738b97a315b08aa983292b8358dcdf51c50a7fe
Loup8ng <valentin.welsch7@gmail.com>
2025-04-07 15:35:05 +0200

    création du code de l'application calculatrice

--- Scripts/applications/Calculator_app.py
+++ Scripts/applications/Calculator_app.py
@@ -1,1 +1,12 @@
 #applications calculatrice
+def calul (ch1:int, ch2: int, opération :chr ): 
+    if opération == "-":
+        résultat =  ch1-ch2
+    elif opération == "+": 
+        résultat= ch1+ch2
+    elif opération == "*": 
+        résultat = ch1*ch2
+    elif opération == "/": 
+        résultat = ch1/ch2
+
+    return résultat
