cae39feefb9a15bd2c65dd0be1a565ffe1ca131f
Loup8ng <valentin.welsch7@gmail.com>
2025-04-18 15:27:05 +0200

    sauvegarde de l'appli_calculette

--- Scripts/applications/Calculator_app.py
+++ Scripts/applications/Calculator_app.py
@@ -62,15 +62,3 @@ if __name__ == "__main__":
     app = Calculatrice()
     app.mainloop()
 
-#applications calculatrice
-def calul (ch1:float, ch2: float, opération :chr ): 
-    if opération == "-":
-        résultat =  ch1-ch2
-    elif opération == "+": 
-        résultat= ch1+ch2
-    elif opération == "*": 
-        résultat = ch1*ch2
-    elif opération == "/": 
-        résultat = ch1/ch2
-
-    return résultat
